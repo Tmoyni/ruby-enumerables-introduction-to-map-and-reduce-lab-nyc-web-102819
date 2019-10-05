f7242d71c215b8f0a566f0940a2d79a5a350070c
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 01:00:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,11 +43,12 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point= 0)
-  
+  reduce = 
   i = 0
   while i < source_array.length do
     source_array[i]
     i += 1
+   end
    
 end 
   
